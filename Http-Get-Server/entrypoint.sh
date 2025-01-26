912ed9e564f420d6d1e5cb841cf5a220cc18399a
Ahmed Yehia <98207790+AhmedHosny2@users.noreply.github.com>
2025-01-26 15:07:06 +0100

    formatting

--- Http-Get-Server/entrypoint.sh
+++ Http-Get-Server/entrypoint.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+  #!/bin/sh
 
-# Run the Python app
-echo "Starting the Python application..."
-python app.py
-exec "$@"
+  # Run the Python app
+  echo "Starting the Python application..."
+  python app.py
+  exec "$@"
